59ff7bd3c3014f83ff9f24eb9fc2fc3762db6f0a
Alireza Rezvani <reza.rezvani73@googlemail.com>
2019-04-02 18:10:39 +0200

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "VuePress",
+  description: "VuePress starter template for CodeSandbox",
+  theme: "@vuepress/vue",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "About Us", link: "/about/" },
+      { text: "VuePress", link: "https://vuepress.vuejs.org/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/vicbergquist/codesandbox-vuepress/"
+      }
+    ]
+  }
+};
